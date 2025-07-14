42997bfee91255fcd5692e3d1cb1ba6ff50e4935
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-07-13 23:24:39 -0600

    feat: add polygon 1

--- filling-polygon/src/main.rs
+++ filling-polygon/src/main.rs
@@ -6,6 +6,9 @@ mod line;
 use raylib::prelude::*;
 use framebuffer::Framebuffer;
 
+mod polygon;
+use polygon::{draw_polygon_outline, fill_polygon_scanline};
+
 fn main() {
     let width = 800;
     let height = 600;
@@ -15,6 +18,25 @@ fn main() {
 
     framebuffer.set_background_color(Color::BLACK);
     framebuffer.clear();
+
+    let polygon1 = vec![
+        Vector2::new(165.0, 380.0),
+        Vector2::new(185.0, 360.0),
+        Vector2::new(180.0, 330.0),
+        Vector2::new(207.0, 345.0),
+        Vector2::new(233.0, 330.0),
+        Vector2::new(230.0, 360.0),
+        Vector2::new(250.0, 380.0),
+        Vector2::new(220.0, 385.0),
+        Vector2::new(205.0, 410.0),
+        Vector2::new(193.0, 383.0),
+    ];
+
+    framebuffer.set_current_color(Color::YELLOW);
+    fill_polygon_scanline(&mut framebuffer, &polygon1);
+
+    framebuffer.set_current_color(Color::WHITE);
+    draw_polygon_outline(&mut framebuffer, &polygon1);
     
     let mut output_file = "out.bmp";
     framebuffer.render_to_file(output_file);
