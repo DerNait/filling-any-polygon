6679cba034dae6d69af85ab7b143e99bc5b6070d
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-07-13 23:28:47 -0600

    feat: add polygon 2

--- filling-polygon/src/main.rs
+++ filling-polygon/src/main.rs
@@ -37,6 +37,19 @@ fn main() {
 
     framebuffer.set_current_color(Color::WHITE);
     draw_polygon_outline(&mut framebuffer, &polygon1);
+
+    let polygon2 = vec![
+        Vector2::new(321.0, 335.0),
+        Vector2::new(288.0, 286.0),
+        Vector2::new(339.0, 251.0),
+        Vector2::new(374.0, 302.0),
+    ];
+
+    framebuffer.set_current_color(Color::BLUE);
+    fill_polygon_scanline(&mut framebuffer, &polygon2);
+    
+    framebuffer.set_current_color(Color::WHITE);
+    draw_polygon_outline(&mut framebuffer, &polygon2);
     
     let mut output_file = "out.bmp";
     framebuffer.render_to_file(output_file);
